d3619917d227137330750badd4240fa2b9a195de
MelleB <265437+MelleB@users.noreply.github.com>
2025-10-28 20:42:26 +0100

    Add devenv + mdbook-gitinfo

---
+++ devenv.nix
@@ -0,0 +1,25 @@
+{ pkgs, lib, ... }:
+let
+  mdbook-gitinfo = pkgs.rustPlatform.buildRustPackage rec {
+    pname = "mdbook-gitinfo";
+    version = "1.1.0";
+    src = pkgs.fetchFromGitHub {
+      owner = "compeng0001";
+      repo  = pname;
+      rev   = "v${version}";
+      sha256 = "sha256-DCn1ArXsawezYRI6OaZ8JS+JiR2tO19Q+KW/oAuMmIk=";
+    };
+    cargoHash = "sha256-IB/I3rNebMLlyGZeryp6xfWGdPWuIJNbVKUyNzGmpGk=";
+    doCheck = false;
+  };
+in
+{
+  packages = [
+    pkgs.pre-commit
+    pkgs.go_1_24
+    pkgs.golangci-lint
+    pkgs.mdbook
+    pkgs.shellcheck
+    mdbook-gitinfo
+  ];
+}
